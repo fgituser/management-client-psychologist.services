691c3e0b0d1e4a675a66aee6c739bd9a853f1e5d
localadmin <farexy@yandex.ru>
2020-04-06 14:17:47 +0700

    refactoring

--- psychologist/internal/service/httperror.go
+++ psychologist/internal/service/httperror.go
@@ -1,13 +1,36 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+	"runtime"
+	"strings"
 
 	"github.com/go-chi/render"
 )
 
-func (rs *restserver) sendErrorJSON(w http.ResponseWriter, r *http.Request, httpStatusCode int, err error) {
-	rs.logger.Error(err)
+func (rs *restserver) sendErrorJSON(w http.ResponseWriter, r *http.Request, httpStatusCode int, msg string, err error) {
+	rs.logger.Errorf(errDetailsMsg(r, httpStatusCode, err, msg, httpStatusCode))
 	render.Status(r, httpStatusCode)
-	render.JSON(w, r, err.Error())
+	render.JSON(w, r, map[string]interface{}{"error": msg})
+}
+
+func errDetailsMsg(r *http.Request, httpStatusCode int, err error, details string, errCode int) string {
+	uinfoStr := ""
+	q := r.URL.String()
+	if qun, e := url.QueryUnescape(q); e == nil {
+		q = qun
+	}
+
+	srcFileInfo := ""
+	if pc, file, line, ok := runtime.Caller(2); ok {
+		fnameElems := strings.Split(file, "/")
+		funcNameElems := strings.Split(runtime.FuncForPC(pc).Name(), "/")
+		srcFileInfo = fmt.Sprintf("[%s:%d %s]", strings.Join(fnameElems[len(fnameElems)-3:], "/"),
+			line, funcNameElems[len(funcNameElems)-1])
+	}
+
+	return fmt.Sprintf("%s - %v - %d (%d) - %s%s - %s",
+		details, err, httpStatusCode, errCode, uinfoStr, q, srcFileInfo)
 }
--- psychologist/internal/service/rest.go
+++ psychologist/internal/service/rest.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/fgituser/management-client-psychologist.services/psychologist/internal/model"
 	"github.com/fgituser/management-client-psychologist.services/psychologist/internal/store"
@@ -49,8 +50,18 @@ func (rs *restserver) configureRouter() {
 			remployees.Use(rs.checkRole)
 			remployees.Get("/employees/{employee_id}/clients/name", rs.clientsNameByEmployeeID)
 			remployees.Get("/employees/{employee_id}/clients/lessons", rs.lessonListByEmployeeID)
+			remployees.Group(func(remployed chi.Router) {
+				remployed.Use(rs.checkAttachment)
+				remployed.Post("/employess/{employee_id}/clients/{client_id}/lessons/date_time/{date_time}/set", rs.lessonSet)
+			})
 		})
 	})
+}
+
+//Schedule an activity with your client. Recording is possible at any time, including non-working
+func (rs *restserver) lessonSet(w http.ResponseWriter, r *http.Request) {
+	// employeeID := chi.URLParam(r, "employee_id")
+	// xrole := r.Header.Get("X-User-Role")
 }
 
 //clientsNameByEmployeeID Get a list of your customer names.
@@ -59,12 +70,12 @@ func (rs *restserver) clientsNameByEmployeeID(w http.ResponseWriter, r *http.Req
 	xrole := r.Header.Get("X-User-Role")
 	clientsID, err := rs.store.FindClients(employeeID)
 	if err != nil {
-		rs.sendErrorJSON(w, r, 500, err)
+		rs.sendErrorJSON(w, r, 500, ErrInternal, err)
 		return
 	}
 	clientsIDNames, err := rs.transportClient.GetNamesByID(clientsID, employeeID, xrole)
 	if err != nil {
-		rs.sendErrorJSON(w, r, 500, err)
+		rs.sendErrorJSON(w, r, 500, ErrInternal, err)
 		return
 	}
 	render.JSON(w, r, clientsIDNames)
@@ -76,12 +87,12 @@ func (rs *restserver) lessonListByEmployeeID(w http.ResponseWriter, r *http.Requ
 	xrole := r.Header.Get("X-User-Role")
 	ll, err := rs.store.LessonsList(employeeID)
 	if err != nil {
-		rs.sendErrorJSON(w, r, 500, err)
+		rs.sendErrorJSON(w, r, 500, ErrInternal, err)
 		return
 	}
 	clientsName, err := rs.transportClient.GetNamesByID(employmentToClientID(ll), employeeID, xrole)
 	if err != nil {
-		rs.sendErrorJSON(w, r, 500, err)
+		rs.sendErrorJSON(w, r, 500, ErrInternal, err)
 		return
 	}
 	for _, l := range ll {
@@ -97,11 +108,56 @@ func (rs *restserver) lessonListByEmployeeID(w http.ResponseWriter, r *http.Requ
 	render.JSON(w, r, ll)
 }
 
+func (rs *restserver) checkAttachment(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		employeeID := chi.URLParam(r, "employee_id")
+		clientID := chi.URLParam(r, "client_id")
+
+		isAttachment, err := rs.store.CheckClientAttachment(employeeID, clientID)
+		if err != nil {
+			rs.sendErrorJSON(w, r, 500, ErrInternal, err)
+			return
+		}
+		if !isAttachment {
+			rs.sendErrorJSON(w, r, 400, "clent not attachment to psychologist", nil)
+			return
+		}
+
+		next.ServeHTTP(w, r.WithContext(r.Context()))
+	})
+}
+
+func (rs *restserver) lessonIsBusy(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		employeeID := chi.URLParam(r, "employee_id")
+		dateTime := chi.URLParam(r, "date_time")
+
+		dd, err := time.Parse("2006-01-02 15:04:05", dateTime)
+		if err != nil {
+			rs.sendErrorJSON(w, r, 500, ErrInternal, err)
+			return
+		}
+
+		isBusy, err := rs.store.LessonIsBusy(employeeID, dd)
+		if err != nil {
+			rs.sendErrorJSON(w, r, 500, ErrInternal, err)
+			return
+		}
+
+		if isBusy {
+			rs.sendErrorJSON(w, r, 400, "lesson is busy", nil)
+			return
+		}
+
+		next.ServeHTTP(w, r.WithContext(r.Context()))
+	})
+}
+
 func (rs *restserver) checkoEmploeeID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		employeeID := chi.URLParam(r, "employee_id")
 		if strings.TrimSpace(employeeID) == "" {
-			rs.sendErrorJSON(w, r, 403, errors.New("not valid employee_id"))
+			rs.sendErrorJSON(w, r, 403, ErrNoAccess, errors.New("not valid employee_id"))
 			return
 		}
 		next.ServeHTTP(w, r.WithContext(r.Context()))
@@ -112,7 +168,7 @@ func (rs *restserver) checkRole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		xrole := r.Header.Get("X-User-Role")
 		if strings.TrimSpace(xrole) == "" {
-			rs.sendErrorJSON(w, r, 403, errors.New("not valid employee_id"))
+			rs.sendErrorJSON(w, r, 403, ErrNoAccess, errors.New("not valid employee_id"))
 			return
 		}
 		for _, ur := range rs.userRoles {
@@ -121,7 +177,7 @@ func (rs *restserver) checkRole(next http.Handler) http.Handler {
 				return
 			}
 		}
-		rs.sendErrorJSON(w, r, 403, errors.New("not valid X-User-Role"))
+		rs.sendErrorJSON(w, r, 403, ErrNoAccess, errors.New("not valid X-User-Role"))
 	})
 }
 
