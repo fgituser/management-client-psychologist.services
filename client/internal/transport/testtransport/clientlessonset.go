9c11a8d18568efeb82bcf094585b8aa93d1de36e
localadmin <farexy@yandex.ru>
2020-04-12 09:55:20 +0700

    fix http test setLesson

--- client/internal/transport/testtransport/clientlessonset.go
+++ client/internal/transport/testtransport/clientlessonset.go
@@ -8,7 +8,7 @@ import (
 
 //ClientLessonSet ...
 func (h *HTTPClient) ClientLessonSet(clientID, psychologistID, userRole string, dateTime time.Time) error {
-	if strings.TrimSpace(psychologistID) != "80d2cdd6-cf69-44e7-9b28-c47792505d81" ||
+	if strings.TrimSpace(psychologistID) != "58faa486-8e73-4c31-b10f-c7f24c115cda"||
 		strings.TrimSpace(clientID) != "75d2cdd6-cf69-44e7-9b28-c47792505d81" {
 		return errors.New("an error occured set lesson")
 	}
--- client/internal/transport/testtransport/clientlessonset_test.go
+++ client/internal/transport/testtransport/clientlessonset_test.go
@@ -21,13 +21,13 @@ func TestHTTPClient_ClientLessonSet(t *testing.T) {
 		{
 			name:    "valid",
 			h:       New(),
-			args:    args{clientID: "75d2cdd6-cf69-44e7-9b28-c47792505d81", psychologistID: "80d2cdd6-cf69-44e7-9b28-c47792505d81", userRole: "client", dateTime: time.Date(2020, 3, 31, 0, 0, 0, 0, time.UTC)},
+			args:    args{clientID: "75d2cdd6-cf69-44e7-9b28-c47792505d81", psychologistID: "58faa486-8e73-4c31-b10f-c7f24c115cda", userRole: "client", dateTime: time.Date(2020, 3, 31, 0, 0, 0, 0, time.UTC)},
 			wantErr: false,
 		},
 		{
 			name:    "not valid clientDI",
 			h:       New(),
-			args:    args{clientID: " ", psychologistID: "80d2cdd6-cf69-44e7-9b28-c47792505d81", userRole: "client", dateTime: time.Date(2020, 3, 31, 0, 0, 0, 0, time.UTC)},
+			args:    args{clientID: " ", psychologistID: "58faa486-8e73-4c31-b10f-c7f24c115cda", userRole: "client", dateTime: time.Date(2020, 3, 31, 0, 0, 0, 0, time.UTC)},
 			wantErr: true,
 		},
 		{
